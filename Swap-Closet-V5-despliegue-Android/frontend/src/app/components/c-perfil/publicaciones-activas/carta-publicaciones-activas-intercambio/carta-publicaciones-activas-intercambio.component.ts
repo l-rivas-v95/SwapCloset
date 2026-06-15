f7b2a141920a080cf37a13998028164c74998127
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-15 19:23:39 +0200

    Limpia import antiguo de carta publicaciones activas

--- Swap-Closet-V5-despliegue-Android/frontend/src/app/components/c-perfil/publicaciones-activas/carta-publicaciones-activas-intercambio/carta-publicaciones-activas-intercambio.component.ts
+++ Swap-Closet-V5-despliegue-Android/frontend/src/app/components/c-perfil/publicaciones-activas/carta-publicaciones-activas-intercambio/carta-publicaciones-activas-intercambio.component.ts
@@ -1,33 +1,28 @@
-import {Component, effect, inject, Input, OnInit, Signal, signal} from '@angular/core';
+import {Component, inject, Input, OnInit, signal} from '@angular/core';
 import {IonicModule} from "@ionic/angular";
-import {UsuarioDTO} from "../../../../modelos/UsuarioDTO";
 import {ProductoService} from "../../../../service/productoService/producto.service";
-import {Observable} from "rxjs";
 import {ProductoDTO} from "../../../../modelos/ProductoDTO";
-import {AnuncioIntercambioPage} from "../../../../pages/anuncio-intercambio/anuncio-intercambio.page";
 import {RouterLink} from "@angular/router";
 import {NgClass} from "@angular/common";
 
 @Component({
-    selector: 'app-carta-publicaciones-activas-intercambio',
-    templateUrl: './carta-publicaciones-activas-intercambio.component.html',
-    styleUrls: ['./carta-publicaciones-activas-intercambio.component.scss'],
-    standalone: true,
+  selector: 'app-carta-publicaciones-activas-intercambio',
+  templateUrl: './carta-publicaciones-activas-intercambio.component.html',
+  styleUrls: ['./carta-publicaciones-activas-intercambio.component.scss'],
+  standalone: true,
   imports: [
     IonicModule,
-    AnuncioIntercambioPage,
     RouterLink,
     NgClass
   ]
 })
-export class CartaPublicacionesActivasIntercambioComponent  implements OnInit {
+export class CartaPublicacionesActivasIntercambioComponent implements OnInit {
 
   @Input() producto!: ProductoDTO;
 
   productos = signal<ProductoDTO[]>([]);
   private productoService = inject(ProductoService);
 
-  // ✅ Usamos directamente el id que recibimos como parámetro
   private cargarProductos() {
     const id = this.producto.idUsuario!;
     this.productoService.getProductosByUsuario(id).subscribe({
@@ -43,9 +38,8 @@ export class CartaPublicacionesActivasIntercambioComponent  implements OnInit {
     }
     return listImagenes[0]?.urlImg || "assets/icon/card-media.png";
   }
+
   ngOnInit(): void {
-    this.cargarProductos()
+    this.cargarProductos();
   }
-
-
 }
