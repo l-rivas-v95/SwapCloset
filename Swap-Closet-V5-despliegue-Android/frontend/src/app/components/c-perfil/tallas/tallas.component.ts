04b0f37f9c742cabb072129f211b63d7bed77fe5
l-rivas-v95 <luisrivi95@gmail.com>
2026-06-15 18:38:55 +0200

    Diferencia edicion de tallas entre perfil propio y ajeno

--- Swap-Closet-V5-despliegue-Android/frontend/src/app/components/c-perfil/tallas/tallas.component.ts
+++ Swap-Closet-V5-despliegue-Android/frontend/src/app/components/c-perfil/tallas/tallas.component.ts
@@ -1,6 +1,6 @@
-import {Component, EventEmitter, input, Input, Output, Signal, signal} from '@angular/core';
-import { IonicModule, ActionSheetController } from '@ionic/angular';
-import { NgIf } from '@angular/common';
+import {Component, EventEmitter, Input, Output, Signal} from '@angular/core';
+import {IonicModule, ActionSheetController} from '@ionic/angular';
+import {NgIf} from '@angular/common';
 import {UsuarioDTO} from "../../../modelos/UsuarioDTO";
 
 @Component({
@@ -12,18 +12,20 @@ import {UsuarioDTO} from "../../../modelos/UsuarioDTO";
 })
 export class TallasComponent {
 
-
-  @Input({ required: true }) usuario!: Signal<UsuarioDTO | null>;
+  @Input({required: true}) usuario!: Signal<UsuarioDTO | null>;
+  @Input() esMiPerfil = true;
   @Output() usuarioChange = new EventEmitter<UsuarioDTO>();
 
   constructor(private actionSheetCtrl: ActionSheetController) {}
 
   async elegirTalla(tipo: 'tCamiseta' | 'tPantalon' | 'tCalzado') {
+    if (!this.esMiPerfil) return;
+
     let opciones: string[] = [];
 
     if (tipo === 'tCamiseta') opciones = ['XS', 'S', 'M', 'L', 'XL'];
     if (tipo === 'tPantalon') opciones = ['38', '40', '42', '44', '46'];
-    if (tipo === 'tCalzado')  opciones = ['36','37','38','39','40','41','42','43'];
+    if (tipo === 'tCalzado') opciones = ['36', '37', '38', '39', '40', '41', '42', '43'];
 
     const botones = opciones.map(talla => ({
       text: talla,
@@ -31,19 +33,16 @@ export class TallasComponent {
         const u = this.usuario();
         if (!u) return;
 
-        const actualizado = { ...u, [tipo]: talla };
-
-        // 🔥 Devolvemos el usuario actualizado al padre
+        const actualizado = {...u, [tipo]: talla};
         this.usuarioChange.emit(actualizado);
       }
     }));
 
     const actionSheet = await this.actionSheetCtrl.create({
       header: `Selecciona talla`,
-      buttons: [...botones, { text: 'Cancelar', role: 'cancel' }]
+      buttons: [...botones, {text: 'Cancelar', role: 'cancel'}]
     });
 
     await actionSheet.present();
   }
-
 }
