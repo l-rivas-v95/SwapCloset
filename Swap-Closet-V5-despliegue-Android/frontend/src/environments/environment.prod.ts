870ff9ab6fc6b6ec3c84381888695b1986e993f4
Luis Rivas Villa <luisrivi95@gmail.com>
2026-06-18 17:48:47 +0200

    Listo para desplegar

--- Swap-Closet-V5-despliegue-Android/frontend/src/environments/environment.prod.ts
+++ Swap-Closet-V5-despliegue-Android/frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://proyecto-swapcloset-luis-rivas-villa.onrender.com/api'
+  apiUrl: 'http://62.84.176.37:8080/api'
+  // apiUrl: 'https://proyecto-swapcloset-luis-rivas-villa.onrender.com/api'
 };
